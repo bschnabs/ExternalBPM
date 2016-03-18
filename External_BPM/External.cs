9b14b2503aed89a6a1bd0cdb93b57dcdcfb7c456
Bob Schnable <rschnable@saberlogic.com>
2016-03-18 10:34:43 -0400

    Initial commit;

---
+++ External_BPM/External.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using Epicor.ServiceModel;
+using Epicor.Data;
+using Epicor.Hosting;
+using Ice.Contracts;
+using Ice.ExtendedData;
+using Ice.Lib;
+using Ice.Tables;
+using Ice.Tablesets;
+
+namespace External //rename if desired
+{
+    public class External //rename if desired
+    {
+        //paste code from method copied here
+    }
+}
